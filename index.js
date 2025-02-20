5d269c0e16c79a0ec3196b7a804d0b8e3d1885fc
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-20 11:17:16 +0300

    Deburged the server side fault

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.use(routes);
 
 app.listen(process.env.port || 4000, () => {
     console.log('Server is running on port 4000')
-});//This is to strtr the server to listen on port 4000
+});//This is to start the server to listen on port 4000
 
