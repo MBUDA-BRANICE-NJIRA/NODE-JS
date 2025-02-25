b495ed53dbcfe7b5335f831ad06d7f9937d1841e
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-25 11:32:11 +0300

    Wrote the code responsible for error display message on student controler

--- controlers/studentController.js
+++ controlers/studentController.js
@@ -1,6 +1,16 @@
 const student = require ('../modules/student');      //This the section responsible fror adding a table
+const createError = require('http-errors');            //This is the section responsible for creating an error
 
 modules.exports={
+    getAllstudents: async(req, res, next)=>{
+        try{
+            const result = await Student.find()
+            res.send(result)
+        } catch (error){
+            console.log(error.message);
+        }
+    },
+
     AddStudent: async (req, res, next) => {
         try {
             const student = new Student (req.body);
