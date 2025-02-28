0d120b3367e212eecafb8a22b2cae3b46a12a28c
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-28 07:26:46 +0300

    Worked on the code readability for joi

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,7 +1,8 @@
 ///This will help validate the data that is being sent to the server
+
 const Joi = require('joi');
 
-const authModel = Joi.object().keys({
+const authModel = Joi.object().keys ({
     email: Joi.string().email().lowercase().required(),
     password: Joi.string().min(6).required(),
 })
