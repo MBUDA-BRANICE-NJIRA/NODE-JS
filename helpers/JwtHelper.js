5768f55bd3a1db8cde08fef140a12ced0f8f179f
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-04 10:47:23 +0300

    Wrote the code for the tockn generation

---
+++ helpers/JwtHelper.js
@@ -0,0 +1,60 @@
+const JWT = require('jsonwebtoken');
+const createError = require('http-errors');
+
+const {authModel} = require('../models/authModel');
+
+module.exports = {
+    signAccessToken:(UserId)=>{
+        return new Promise((resolve, reject)=>{
+            const payload = {};
+            const secret = process.env.ACCESS_TOKEN_SECRET;
+           const options ={
+            expiresIn: '1h',
+               issuer: 'EddTechnologies.com',
+            audience: UserId,
+           }
+           JWT.sign(payload,secret,options,(error,token)=>{
+            if(error){
+                console.log(error.message);
+                reject(createError.InternalServerError());
+            }
+            resolve(token);
+           })
+        })
+    },
+
+    signRefreshToken:(UserId)=>{
+        return new Promise((resolve,reject)=>{
+            const payload ={};
+            const secret = process.env.REFRESH_TOKEN_SECRET;
+            const options ={
+                expiresIn: '6s',
+                issuer: 'EddiTechnologies.com',
+                audience: UserId,
+            }
+         JWT.sign(payload,secret,options,(error,token)=>{
+           if(error){
+               console.log(error.message);
+               reject(createError.InternalServerError());
+           }
+           resolve(token);
+         })
+        })
+    },
+
+    // verifyAccessToken:(request,respond, next)=>{
+    //     if(!request.headers['authorization']) return next(createError.Unauthorized())
+    //         const authHeader = request.headers['authorization']
+    //     const bearerToken = authHeader.split(' ');
+    //     const token = bearerToken[1];
+    //     JWT.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,payload)=>{
+    //         if(err){
+    //             if(err.name === 'jsonWebTokenError'){
+    //                 return next(createError.Unauthorized('err.message'));
+    //             }
+    //         }
+    //         request.payload = payload;
+    //         next();
+    //     })
+    // }
+}
